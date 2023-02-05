037aa65716cab7181a7f718d1dc7e9f0d82d8ef0
0xRenegade <therenegadecoding@gmail.com>
2023-02-05 09:30:43 -0600

    add 404 page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <h1>404</h1>
+        <p>The page you requested could not be found.</p>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
