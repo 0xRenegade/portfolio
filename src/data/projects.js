6aab43cecf6f8d714677a1d2375e339cb56173e8
0xRenegade <therenegadecoding@gmail.com>
2023-02-08 23:06:37 -0600

    add more info

--- src/data/projects.js
+++ src/data/projects.js
@@ -15,4 +15,16 @@ export const projects = [
     image:
       'https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80',
   },
+  {
+    slug: 'open-source',
+    title: 'Open Source Contributions',
+    description: "https://github.com/0xRenegade/PHP-Dynamic-Qr-code | A QRCode Generator with dashboard I added features to.",
+    image: "https://camo.githubusercontent.com/4c5229bf7e590f5ea77d5e8730dd8a1ee954ec968d43746a546c574928050dd7/68747470733a2f2f7777772e6769616e646f6e61746f696e766572736f2e69742f646f63756d656e746174696f6e2f6173736574732f44796e616d69635152436f64655f4f726967696e616c2e706e67"
+  },
+  {
+    slug: 'survey-builder',
+    title: 'Survey Builder',
+    description: "A web application in which subscribed users could visually build surveys to send to a list of users/emails to deliver. I built a site owner's administrator UI and extended other features onto the application.",
+    image: "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=81"
+  }
 ]
