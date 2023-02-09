4e2c8d267af3c69757c862c8d09cf5c6b3af1c8c
0xRenegade <therenegadecoding@gmail.com>
2023-02-08 22:45:23 -0600

    add project info

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,18 @@
-const placeHolderImage =
-  'https://images.unsplash.com/photo-1515787366009-7cbdd2dc587b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80';
-const placeHolderText =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-
 export const projects = [
   {
-    slug: 'project-one',
-    title: 'Web App One',
-    description: placeHolderText,
-    image: placeHolderImage,
+    slug: 'loyalty-program',
+    title: 'Loyalty Program',
+    description:
+      'A core PHP/MySQL web application that processes loyalty rewards on an online POS system for brick and mortar stores, as ecommerce platforms such as Shopify and BigCommerce.',
+    image:
+      'https://images.unsplash.com/photo-1554774853-b415df9eeb92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
   },
   {
-    slug: 'project-two',
-    title: 'Web App Two',
-    description: placeHolderText,
-    image: placeHolderImage,
+    slug: 'loans-program',
+    title: 'Loan Application Customer Service Web Application',
+    description:
+      'Now say that 5 times, fast. All jokes aside, this web application is used by loan officers to process applications for procuring loans/funding.',
+    image:
+      'https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80',
   },
-];
+]
