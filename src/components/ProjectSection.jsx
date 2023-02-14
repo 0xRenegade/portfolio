83bd31f00e253bc506815ee464529d994dad951b
0xRenegade <therenegadecoding@gmail.com>
2023-02-14 11:34:07 -0600

    add in websites card

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -39,6 +39,34 @@ const ProjectSection = () => {
             </Col>
           )
         })}
+      <Col
+        xs={12}
+        sm={12}
+        md={12}
+        lg={6}
+        xl={6}
+        xxl={6}
+        className="d-flex justify-content-center align-items-center pl-1 pr-1"
+      >
+        <Link className="project-card" to={'/websites/'}>
+          <Card
+            style={{
+              width: '18rem',
+            }}
+          >
+            <Card.Img
+              variant="top"
+              src={
+                'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
+              }
+            />
+            <Card.Body>
+              <Card.Title>WordPress Websites</Card.Title>
+              <Card.Text>Click here to see my WordPress projects</Card.Text>
+            </Card.Body>
+          </Card>
+        </Link>
+      </Col>
     </Row>
   )
 }
