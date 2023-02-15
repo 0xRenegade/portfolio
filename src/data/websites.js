0d7a5d63707c064bfefa9d8b5842ad76b232a39e
0xRenegade <therenegadecoding@gmail.com>
2023-02-15 11:50:14 -0600

    add websites data

---
+++ src/data/websites.js
@@ -0,0 +1,40 @@
+import peakImg from '../assets/peak-rewards.png'
+import peterImg from '../assets/peters-clothiers.png'
+import kenImg from '../assets/kens-mans-shop.svg'
+import martinoImg from '../assets/martinos-chicago.png'
+import pieProjectImg from '../assets/pie-project.png'
+import loveitImg from '../assets/loveit-label.png'
+
+export const websites = [
+  {
+    title: 'Peak Rewards',
+    url: 'https://peakrewards.com/',
+    image: peakImg,
+  },
+  {
+    title: "Peter's Clothiers",
+    url: 'https://petersclothiers.com/',
+    image: peterImg,
+  },
+  {
+    title: "Ken's Mans Shop",
+    url: "https://kensmansshop.com/",
+    image: kenImg,
+  },
+  {
+    title: "Martino's Chicago Pizza",
+    url: "http://martinoschicago.com/",
+    image:
+      martinoImg,
+  },
+  {
+    title: "The PiE Project",
+    url: "https://www.thepieproject.org/",
+    image: pieProjectImg
+  },
+  {
+    title: "Love It Labels",
+    url: "https://loveitlabels.com/",
+    image: loveitImg
+  }
+]
